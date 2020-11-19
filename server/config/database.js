76b57272357521652eaad6a99571be8f61b55f1d
Alfath Dioni <fathalfath30@gmail.com>
2020-11-19 21:53:36 +0700

    update database config

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,24 @@
-{
-  "username": "root",
-  "password": null,
-  "database": "scheduling_apps",
-  "host": "127.0.0.1",
-  "dialect": "mysql"
+/*
+//
+//  ______    _   _           _  __      _   _     ____   ___
+// |  ____|  | | | |         | |/ _|    | | | |   |___ \ / _ \
+// | |__ __ _| |_| |__   __ _| | |_ __ _| |_| |__   __) | | | |
+// |  __/ _` | __| '_ \ / _` | |  _/ _` | __| '_ \ |__ <| | | |
+// | | | (_| | |_| | | | (_| | | || (_| | |_| | | |___) | |_| |
+// |_|  \__,_|\__|_| |_|\__,_|_|_| \__,_|\__|_| |_|____/ \___/
+//
+// Written by Fathalfath30.
+// Email : fathalfath30@gmail.com
+// Follow me on:
+//  Github : https://github.com/fathalfath30
+//  Gitlab : https://gitlab.com/Fathalfath30
+//
+*/
+
+module.exports = {
+  'username': 'root',
+  'password': null,
+  'database': 'scheduling_apps',
+  'host': '127.0.0.1',
+  'dialect': 'mysql',
 }
